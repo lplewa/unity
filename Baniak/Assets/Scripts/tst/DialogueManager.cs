e7c2b8a2ebcbeeee42f4f5c4d87787783013a8cc
Zuthen <winstonivo@gmail.com>
2020-03-27 21:18:13 +0100

    Mechanizm dialogowy dla NPCów

---
+++ Baniak/Assets/Scripts/tst/DialogueManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+    public Animator dialogueAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(NPCDialogue dialogue)
+    {
+        Debug.Log("Starting conversation with " + dialogue.name);
+        dialogueAnimator.SetBool("isOpen", true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines(); //żeby nam się animacja nie zamknęła podczas przewijania tekstu
+        StartCoroutine(TypeSentence(sentence));
+    }
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+
+    void EndDialogue()
+    {
+        Debug.Log("End of conversation");
+        dialogueAnimator.SetBool("isOpen", false);
+    }
+}
---
+++ Baniak/Assets/Scripts/tst/NPC.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class NPC : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+    public NPCDialogue dialogue;
+    public bool isSpeaking;
+
+    private void Start()
+    {
+        dialogueManager = GetComponent<DialogueManager>();
+        dialogue = GetComponent<DialogueTrigger>().dialogue;
+        isSpeaking = false;
+    }
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            this.dialogueManager.DisplayNextSentence();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("NPC Collision");
+        isSpeaking = true;
+        this.dialogueManager.StartDialogue(dialogue);
+    }
+
+}

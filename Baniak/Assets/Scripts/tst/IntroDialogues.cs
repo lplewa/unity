57c691a01c1795e9736ce9562fcb854fade30df9
Zuthen <winstonivo@gmail.com>
2020-03-28 10:08:32 +0100

    Dodanie dialogów do wszystkich gadających obiektów

---
+++ Baniak/Assets/Scripts/tst/IntroDialogues.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroDialogues : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+    public NPCDialogue dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueManager = GetComponent<DialogueManager>();
+        dialogue = GetComponent<DialogueTrigger>().dialogue;
+        this.dialogueManager.StartDialogue(dialogue);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            this.dialogueManager.DisplayNextSentence();
+        }
+    }
+}
--- Baniak/Assets/Scripts/tst/NPC.cs
+++ Baniak/Assets/Scripts/tst/NPC.cs
@@ -5,29 +5,52 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    public DialogueManager dialogueManager;
+    private DialogueManager dialogueManager;
     public NPCDialogue dialogue;
-    public bool isSpeaking;
+    private bool dialogueStarted;
 
     private void Start()
     {
         dialogueManager = GetComponent<DialogueManager>();
         dialogue = GetComponent<DialogueTrigger>().dialogue;
-        isSpeaking = false;
+        dialogueStarted = false;
     }
     private void Update()
     {
-        if (Input.anyKeyDown)
+        if (dialogueStarted)
         {
-            this.dialogueManager.DisplayNextSentence();
+            if (Input.GetKeyDown("space"))
+            {
+                this.dialogueManager.DisplayNextSentence();
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("NPC Collision");
-        isSpeaking = true;
         this.dialogueManager.StartDialogue(dialogue);
+        dialogueStarted = true;
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("NPC Collision");
+        this.dialogueManager.StartDialogue(dialogue);
+        dialogueStarted = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Debug.Log("End Collision");
+        dialogueStarted = false;
+        dialogueManager.dialogueAnimator.SetBool("isOpen", false);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Debug.Log("End Trigger");
+        dialogueStarted = false;
+        dialogueManager.dialogueAnimator.SetBool("isOpen", false);
+    }
 }

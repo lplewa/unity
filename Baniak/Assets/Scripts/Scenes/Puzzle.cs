e4890d35b9fc84356d68f5c7359688cf52f5a0dc
Zuthen <winstonivo@gmail.com>
2020-04-17 00:00:11 +0200

    Zagadka Apominusa

---
+++ Baniak/Assets/Scripts/Scenes/Puzzle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puzzle : MonoBehaviour
+{
+    public DialogueManager dialogueManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.gameObject.SetActive(false);
+    }
+
+}
---
+++ Baniak/Assets/Scripts/Scenes/QuizButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuizButton : MonoBehaviour
+{
+    [SerializeField] private bool isGood;
+
+    public void CheckAnswer()
+    {
+        DialogueManager dialogueManager = new DialogueManager();
+        dialogueManager = FindObjectOfType<LevelManager>().npc;
+        if (isGood)
+        {
+            dialogueManager.missionAccomplished = true;
+            Puzzle puzzle = FindObjectOfType<Puzzle>();
+            puzzle.transform.gameObject.SetActive(false);
+
+        }
+        dialogueManager.GetComponent<NPC>().StartTalking();
+    }
+}

7651a80a595ff2a98b39442da3bfd388117803ba
Zuthen <winstonivo@gmail.com>
2020-04-25 18:06:15 +0200

    Przejście do gralni z pustej tawerny

--- Baniak/Assets/Scripts/LevelManagers/EmptyTavernManager.cs
+++ Baniak/Assets/Scripts/LevelManagers/EmptyTavernManager.cs
@@ -6,27 +6,49 @@ using static Baniak_Controler;
 public class EmptyTavernManager : MonoBehaviour
 {
     public NPC dzieran;
+    public NPC baniakText;
     public Baniak_Controler baniak;
     private bool dzieranSpoken;
+    private bool baniakAnswered;
 
     public void Start()
     {
         dzieranSpoken = false;
+        baniakAnswered = false;
     }
 
     private void Update()
     {
         DzieranSpeach();
+       BaniakResponse();
+        MoveToGamesRoom();
     }
 
     private void DzieranSpeach()
     {
-        if (!dzieranSpoken)
+        if (!dzieran.dialogueManager.missionStarted && !dzieranSpoken)
         {
             baniak.state = State.Talking;
             dzieran.StartTalking();
             dzieranSpoken = true;
-        }
+                    }
+    }
 
+    private void BaniakResponse()
+    {
+        if (dzieranSpoken && !baniakAnswered && dzieran.dialogueManager.missionStarted)
+        {
+            baniak.state = State.Talking;
+            baniakText.StartTalking();
+            baniakAnswered = true;
+        }
+    }
+
+    private void MoveToGamesRoom()
+    {
+        if(dzieran.dialogueManager.missionStarted && baniakText.dialogueManager.missionStarted)
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Games Room Fight");
+        }
     }
 }
